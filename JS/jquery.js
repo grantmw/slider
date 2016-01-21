25551fb74e1f26872cbece997ce430242d071a3d
grantmw <grantmwong@gmail.com>
2016-01-21 15:07:45 -0800

    js - update range display value and position on move

--- JS/jquery.js
+++ JS/jquery.js
@@ -1,5 +1,23 @@
 $(document).ready(function(){
 
+  document.getElementById("slider").oninput = function(){
+    var input_value = $('#slider').val();
+    $('.handle').html(input_value)
+    $('.slidevalue').css('left', input_value + '%')
+    $('.pop').html(input_value)
+
+  };
+
+
+  $('.inputcontainer')
+  .on('mousedown','input', function(){
+    $('.handle').css('display','none')
+    $('.pop').css('display','block')
+  })
+  .on('mouseup','input', function(){
+    $('.handle').css('display','block')
+    $('.pop').css('display','none')
+  });
 
 
 });
